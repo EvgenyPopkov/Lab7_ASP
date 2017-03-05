71015f34988bbd9213b4c88a38aa1ac97b528f28
EvgenyPopkov <popkov_signal18@mail.ru>
2017-03-05 23:01:51 +0500

    Add code

---
+++ TestMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace Lab_ASP
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class TestMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public TestMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            switch (httpContext.Request.Path.Value)
+            {
+                case "/test1":
+                    await httpContext.Response.WriteAsync("Test1");
+                    break;
+
+                case "/test2":
+                    await httpContext.Response.WriteAsync("Test2");
+                    break;
+
+                default:
+                    await httpContext.Response.WriteAsync("Hello world");
+                    break;
+            }
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class TestMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseTestMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<TestMiddleware>();
+        }
+    }
+}
